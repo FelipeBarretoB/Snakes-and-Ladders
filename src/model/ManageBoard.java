11523cea5f847ec6495f78a3cbe2cfdad604b5e4
Martin Cifuentes <marcisego@gmail.com>
2021-04-23 09:13:05 -0500

    players

--- src/model/ManageBoard.java
+++ src/model/ManageBoard.java
@@ -137,7 +137,7 @@ public class ManageBoard {
 		this.m=m;
 		this.s=s;
 		this.e=e;
-		if((int)players.charAt(0) >= 48 && (int)players.charAt(0) >= 58){
+		if((int)players.charAt(0) >= 48 && (int)players.charAt(0) >= 57){
 			this.players = new Players(Integer.parseInt(players));
 		}else {
 			this.players = new Players(players.length(), players);
